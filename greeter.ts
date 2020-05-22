9eb005a7a5faf69a23c42f45720a1f80f1d0d456
Kelvin Maues <kgmdeveloper@gmail.com>
2020-05-22 11:31:16 -0300

    Class concept with TS

--- greeter.ts
+++ greeter.ts
@@ -1,8 +1,19 @@
+// # Classes
+// A class with a few public field
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+  }
+}
 
-// # Interfaces
+// # Interface
 // 1. Two types are compatible if their internal structure is compatible
 // 2. This allows to implement an interface just by having the shape the interface requires.
-
 interface Person {
   firstName: string;
   lastName: string;
@@ -13,7 +24,7 @@ function greeter(person: Person) {
 }
 
 // let user = "Jane User";
-
-let user = { firstName: "Kelvin", lastName: "Maués" };
+// let user = { firstName: "Kelvin", lastName: "Maués" };
+let user = new Student("Kelvin", "G.", "Maués");
 
 document.body.textContent = greeter(user);
