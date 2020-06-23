8df2dd416f732306ccd77aa402c01b600a655f2d
Kelvin Maues <kgmdeveloper@gmail.com>
2020-06-23 20:30:59 -0300

    Decorators:  class, property and method

--- decorators.ts
+++ decorators.ts
@@ -3,17 +3,94 @@
 // @useState
 
 // Factory
-function Logger(prefix: string) {
-  return (target) => {
-    console.log(`${prefix} - ${target}`);
+// function Logger(prefix: string) {
+//   return (target) => {
+//     console.log(`${prefix} - ${target}`);
+//   };
+// }
+
+// @Logger("awesome")
+// class Foo {}
+
+// Class decorator
+function setAPIVersion(apiVersion: string) {
+  return (constructor: any) => {
+    return class extends constructor {
+      version = apiVersion;
+    };
   };
 }
 
-@Logger("awesome")
-class Foo {}
+@setAPIVersion("1.0.0")
+class API {}
 
-// Class decorator
+console.log(new API());
+
 // Property decorator
+// function minLength(len: number) {
+//   return (target: any, key: string) => {
+//     let val = target[key];
+
+//     const getter = () => val;
+
+//     const setter = (value: string) => {
+//       if (value.length < len) {
+//         console.log(`Error: você não pode criar ${key} menor que ${len}`);
+//       } else {
+//         val = value;
+//       }
+//     };
+
+//     Object.defineProperty(target, key, {
+//       get: getter,
+//       set: setter,
+//     });
+//   };
+// }
+
+// class Movie {
+//   // validação - se for menor que 5 - error
+//   @minLength(50)
+//   title: string;
+
+//   constructor(t: string) {
+//     this.title = t;
+//   }
+// }
+
+// const movie = new Movie("Interstellar");
+// console.log(movie.title);
+
 // Method decorator
+function delay(ms: number) {
+  return (target: any, key: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any) {
+      console.log(`waiting ${ms}...`);
+      setTimeout(() => {
+        originalMethod.apply(this, args);
+      }, ms);
+    };
+    return descriptor;
+  };
+}
+
+class Greeter {
+  greeting: string;
+
+  constructor(g: string) {
+    this.greeting = g;
+  }
+
+  @delay(1000)
+  greet() {
+    console.log(`Hello! ${this.greeting}`);
+  }
+}
+
+const pessoinha = new Greeter("pessoinha");
+pessoinha.greet();
+
 // Parameter decorator
 // Accessor decorator
--- dist/decorators.js
+++ dist/decorators.js
@@ -2,6 +2,19 @@
 // @Component
 // @Selector
 // @useState
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -9,21 +22,95 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 // Factory
-function Logger(prefix) {
-    return function (target) {
-        console.log(prefix + " - " + target);
+// function Logger(prefix: string) {
+//   return (target) => {
+//     console.log(`${prefix} - ${target}`);
+//   };
+// }
+// @Logger("awesome")
+// class Foo {}
+// Class decorator
+function setAPIVersion(apiVersion) {
+    return function (constructor) {
+        return /** @class */ (function (_super) {
+            __extends(class_1, _super);
+            function class_1() {
+                var _this = _super !== null && _super.apply(this, arguments) || this;
+                _this.version = apiVersion;
+                return _this;
+            }
+            return class_1;
+        }(constructor));
     };
 }
-var Foo = /** @class */ (function () {
-    function Foo() {
+var API = /** @class */ (function () {
+    function API() {
     }
-    Foo = __decorate([
-        Logger("awesome")
-    ], Foo);
-    return Foo;
+    API = __decorate([
+        setAPIVersion("1.0.0")
+    ], API);
+    return API;
 }());
-// Class decorator
+console.log(new API());
 // Property decorator
+// function minLength(len: number) {
+//   return (target: any, key: string) => {
+//     let val = target[key];
+//     const getter = () => val;
+//     const setter = (value: string) => {
+//       if (value.length < len) {
+//         console.log(`Error: você não pode criar ${key} menor que ${len}`);
+//       } else {
+//         val = value;
+//       }
+//     };
+//     Object.defineProperty(target, key, {
+//       get: getter,
+//       set: setter,
+//     });
+//   };
+// }
+// class Movie {
+//   // validação - se for menor que 5 - error
+//   @minLength(50)
+//   title: string;
+//   constructor(t: string) {
+//     this.title = t;
+//   }
+// }
+// const movie = new Movie("Interstellar");
+// console.log(movie.title);
 // Method decorator
+function delay(ms) {
+    return function (target, key, descriptor) {
+        var originalMethod = descriptor.value;
+        descriptor.value = function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            console.log("waiting " + ms + "...");
+            setTimeout(function () {
+                originalMethod.apply(_this, args);
+            }, ms);
+        };
+        return descriptor;
+    };
+}
+var Greeter = /** @class */ (function () {
+    function Greeter(g) {
+        this.greeting = g;
+    }
+    Greeter.prototype.greet = function () {
+        console.log("Hello! " + this.greeting);
+    };
+    __decorate([
+        delay(1000)
+    ], Greeter.prototype, "greet", null);
+    return Greeter;
+}());
+var pessoinha = new Greeter("pessoinha");
+pessoinha.greet();
 // Parameter decorator
 // Accessor decorator
